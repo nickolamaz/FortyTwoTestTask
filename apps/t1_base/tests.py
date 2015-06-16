f76725af9d4c4fca92057b0838fb20507d8b6891
nickola <nickola_90@list.ru>
2015-06-17 00:09:40 +0300

    ticket: 1 added ModelTest with implementation

--- apps/t1_base/tests.py
+++ apps/t1_base/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from models import Contact
 
 # Create your tests here.
+class ModelTest(TestCase):
+    def setUp(self):
+        self._contact = {
+            "bio": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. \
+            \r\nAenean commodo ligula eget dolor. Aenean massa. \
+            \r\nCum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.",
+            "name": "Nikolay",
+            "contacts": "Nikolaev",
+            "last_name": "Mazurenko",
+            "date_of_birth": "1990-01-18",
+            "other_contacts": "https://github.com/nickolamaz/",
+            "skype": "nickola2007",
+            "jabber": "nickolamaz@jabber.ru",
+            "email": "nickola_90@list.ru",
+        }
+
+        self.contact = Contact.objects.create(**self._contact)
+
+    def test_fields(self):
+        for k in self._contact.keys():
+            self.assertEquals(self._contact[k], getattr(self.contact, k))
