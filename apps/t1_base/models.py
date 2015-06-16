f251f268f8791a680cfd28fd985ff99eae428b1b
nickola <nickola_90@list.ru>
2015-06-16 23:07:49 +0300

    Added Contacts Model, and registered in Admin

---
+++ apps/t1_base/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.forms import ModelForm
+# Create your models here.
+
+class Contact(models.Model):
+    name = models.CharField('Name', max_length=50)
+    last_name = models.CharField('Last name', max_length=50)
+    date_of_birth = models.DateField('Date of birth',db_index=True)
+    bio = models.TextField('Bio', null=1, blank=1)
+    contacts = models.CharField('Contacts', max_length=200)
+    email = models.EmailField('Email')
+    jabber = models.EmailField('Jabber ID')
+    skype = models.CharField('Skype ID', max_length=50, null=1, blank=1)
+    other_contacts = models.TextField('Other contacts', null=1, blank=1)
+
+class ContactForm(ModelForm):
+    class Meta:
+        model = Contact
