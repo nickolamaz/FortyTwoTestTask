6dd3209ac065bc8e7820b8a749f77b848b92e6da
nickola <nickola_90@list.ru>
2015-09-05 19:55:18 +0300

    ticket:13 Added priority field to requests

--- apps/t3_middleware/admin.py
+++ apps/t3_middleware/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from apps.t3_middleware.models import HttpRequestStore
 
 
-admin.site.register(HttpRequestStore)
+class HttpRequestStoreAdmin(admin.ModelAdmin):
+    list_display = ('date', 'method', 'path', 'host', 'priority')
+    list_filter = ['priority']
+
+admin.site.register(HttpRequestStore, HttpRequestStoreAdmin)
