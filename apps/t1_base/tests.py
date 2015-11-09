efb9fbdb6109b37f890f5901d866bb967c4c1ad7
nickola <nickola_90@list.ru>
2015-11-09 12:50:35 +0200

    ticket:1 Add new test

--- apps/t1_base/tests.py
+++ apps/t1_base/tests.py
@@ -10,19 +10,15 @@ class ModelTest(TestCase):
     def setUp(self):
         """Create new person"""
         self._contact = {
-            "bio": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. \
-            \r\nAenean commodo ligula eget dolor. Aenean massa. \
-            \r\nCum sociis natoque penatibus et magnis dis parturient "
-                   "montes, nascetur ridiculus mus.",
             "name": "Nikolay",
-            "contacts": "Nikolaev",
             "last_name": "Mazurenko",
-            "date_of_birth": "1990-01-18",
-            "other_contacts": "https://github.com/nickolamaz/",
-            "skype": "nickola2007",
-            "jabber": "nickolamaz@jabber.ru",
+            "date_of_birth": "2015-09-08",
+            "bio": "Here must be my bio",
+            "contacts": "my_contacts",
             "email": "nickola_90@list.ru",
-            "photo": None,
+            "jabber": "nickolamaz",
+            "skype": "nickolamaz",
+            "other_contacts": "Mazurenko"
         }
 
         self.contact = Contact.objects.create(**self._contact)
@@ -36,3 +32,15 @@ class ModelTest(TestCase):
         """Testing get request for index page"""
         request = self.client.get(reverse(views.index))
         self.assertEqual(request.status_code, 200)
+
+    def test_get_client_info(self):
+        """Testing all fields in response"""
+        request = self.client.get(reverse(views.index))
+        print request
+        self.assertContains(request, self.contact.bio)
+        self.assertContains(request, self.contact.name)
+        self.assertContains(request, self.contact.contacts)
+        self.assertContains(request, self.contact.last_name)
+        self.assertContains(request, self.contact.other_contacts)
+        self.assertContains(request, self.contact.skype)
+
