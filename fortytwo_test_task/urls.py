8eaf9ed7bded6192e5389d08b8254c6c3f2f8f39
nickola <nickola_90@list.ru>
2015-11-09 23:54:14 +0200

    ticket:1 Fixed line length error

--- fortytwo_test_task/urls.py
+++ fortytwo_test_task/urls.py
@@ -18,8 +18,9 @@ urlpatterns = patterns(
     #     {"template_name": "login.html"}),
     # url(r'^logout/', views.logout, name='logout'),
     url(r'^accounts/', include('django.contrib.auth.urls'), name='login'),
-    url(r'^accounts/register/', CreateView.as_view(template_name='registration/register.html',
-                                                   form_class=UserCreationForm,
-                                                   success_url='/'), name='register'),
+    url(r'^accounts/register/', CreateView.as_view(
+        template_name='registration/register.html',
+        form_class=UserCreationForm,
+        success_url='/'), name='register'),
     url(r'^edit/', views.edit, name='edit'),
 )
