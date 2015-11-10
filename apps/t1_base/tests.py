ea370c2777f2467b2538d415ff54fc7b339f3a4d
nickola <nickola_90@list.ru>
2015-11-10 14:25:57 +0200

    ticket:1 Add new tests

--- apps/t1_base/tests.py
+++ apps/t1_base/tests.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from django.test import TestCase
 from models import Contact
 from . import views
@@ -23,3 +24,29 @@ class ModelTest(TestCase):
         self.assertContains(request, self.contact.last_name)
         self.assertContains(request, self.contact.other_contacts)
         self.assertContains(request, self.contact.skype)
+
+    def test_unicode(self):
+        """Testing unicode in response"""
+        self.contact.name = 'Имя'
+        self.contact.last_name = 'Фамилия'
+        self.contact.save()
+        unicode_name = u'Имя'
+        unicode_last_name = u'Фамилия'
+        request = self.client.get(reverse(views.index))
+        self.assertContains(request, 'utf-8')
+        self.assertContains(request, unicode_name)
+        self.assertContains(request, unicode_last_name)
+
+    def test_empty_db(self):
+        """Testing empty database"""
+        Contact.objects.all().delete()
+        request = self.client.get(reverse(views.index))
+        self.assertEqual(request.context['contact'], None)
+
+    def test_many_db_rows(self):
+        """Testing more than one row in database"""
+        Contact.objects.create(name='First Name',
+                               last_name='Last Name',
+                               date_of_birth='1890-10-23')
+        request = self.client.get(reverse(views.index))
+        self.assertEqual(request.context['contact'], Contact.objects.first())
